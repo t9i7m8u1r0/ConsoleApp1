82c0b93f7ed2400bd267d43afac033ee7ef4f9da
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-02-02 04:37:05 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Timur";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
