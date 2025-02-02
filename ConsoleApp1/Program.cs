6125522b98a69b0800485e22fbe91a7b6711052b
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-02-03 03:06:29 +0700

    Задание 3.3.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 
-namespace FirstApplication.ConsoleApp
+class MainClass
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string MyName;
-            MyName = "Timur";
-            Console.WriteLine(MyName);
-            Console.ReadKey();
-        }
+
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+
     }
 }
