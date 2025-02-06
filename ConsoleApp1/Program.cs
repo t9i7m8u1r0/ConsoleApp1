445f81e4c35971c34980a1f6d567bfa65b464e32
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-02-07 03:10:50 +0700

    Задание 4.1.17

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,14 +4,31 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.Write("Enter your name: ");
-        var name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        var age = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Your name is {name} and age is {age}");
-        Console.Write("Enter your birthdate: ");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine($"Your birthdate is {birthdate}");
-        Console.ReadKey();
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        var color = Console.ReadLine();
+
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
     }
 }
